2820912389247fbc7e10c9ef8d1cab3f3e03b110
geniusvale <ariiqromero@gmail.com>
2022-04-22 22:17:58 +0700

    fix pub api

--- lib/api/publikasi_api.dart
+++ lib/api/publikasi_api.dart
@@ -9,10 +9,10 @@ class PublikasiAPI {
         'https://webapi.bps.go.id/v1/api/list/model/publication/domain/3274/key/841396623eaa7ad43906a4a64c96e7a1/');
 
     if (response.statusCode == 200) {
-      List<PublikasiModel> brs = (response.data['data'][1] as List)
+      List<PublikasiModel> publikasi = (response.data['data'][1] as List)
           .map((e) => PublikasiModel.fromJson(e))
           .toList();
-      return brs;
+      return publikasi;
     } else {
       throw Exception('Tidak Ada Publikasi.');
     }
